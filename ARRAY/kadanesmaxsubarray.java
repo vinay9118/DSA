37d60965c78395a2458d0fa91300200f988330d1
veer <vinaykumar24427@gmail.com>
2025-09-24 22:54:12 +0530

    KADAN

---
+++ ARRAY/kadanesmaxsubarray.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class kadanesmaxsubarray{
+    public static int subarray(int [] nums){
+    int currsum=0;
+    int maxsum=Integer.MIN_VALUE;
+    for(int i=0;i<nums.length;i++){
+        currsum=currsum+nums[i];
+        if(currsum<0){
+            currsum=0;
+        }
+        maxsum=Math.max(currsum,maxsum);
+    }
+    return maxsum;
+    }
+  
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size::");
+        int n=sc.nextInt();
+
+        int[] arr= new int[n];
+        for(int i=0;i<arr.length;i++){
+            System.out.print("Enter An Element::");
+            arr[i]=sc.nextInt();
+        }
+        int result=subarray(arr);
+        System.out.print("maximum sum of subarray:"+result); 
+        
+      
+    }
+}
