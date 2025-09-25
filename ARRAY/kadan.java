512dce9012d1534455a1d53fc84ad304175090c8
veer <vinaykumar24427@gmail.com>
2025-09-25 10:23:32 +0530

    KADAN

---
+++ ARRAY/kadan.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class kadan{
+    public static int maxsubsum(int [] nums){
+    int currsum=nums[0];
+    int maxsum=nums[0];
+    for(int i=1;i<nums.length;i++){
+       if(currsum+nums[i]>nums[i]){
+        currsum+=nums[i];
+       }
+       else{
+        currsum=nums[i];
+       }
+       maxsum=Math.max(maxsum,currsum);
+    }
+    return maxsum;
+    }
+  
+    public static void main(String[] args){
+        int [] arr={-2,-3,4,-1,-2,1,5,-3};
+        int result=maxsubsum(arr);
+        System.out.print("maximum sum of subarray:"+result); 
+        
+      
+    }
+}
