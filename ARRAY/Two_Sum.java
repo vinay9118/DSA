3d4667049ffee0002360ce1039a366741db67c18
veer <vinaykumar24427@gmail.com>
2025-09-29 16:08:41 +0530

    Two_Sum

---
+++ ARRAY/Two_Sum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Two_Sum {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end < nums.length; end++) {
+                if (nums[start] + nums[end] == target) {
+                    System.out.println(start + " " + end); 
+                    return new int[] { start, end };
+                }
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args){
+        int[]arr={2,7,11,15};
+        int target=9;
+        int [] result=twoSum(arr,target);
+        System.out.println(Arrays.toString(result));
+    }
+}
