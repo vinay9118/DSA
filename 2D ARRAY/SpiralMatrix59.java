5ac82fd4e0cf35c4b4df118ba06d6ff7e927a034
veer <vinaykumar24427@gmail.com>
2025-10-02 11:34:17 +0530

    Spiral_Matrix

---
+++ 2D ARRAY/SpiralMatrix59.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class SpiralMatrix59{
+    public static int [][] generateMatrix(int n){
+        //create an array size of n*n
+        int [][] arr=new int[n][n];
+        
+        int count=1;
+
+        int minrow=0;
+        int mincol=0;
+        int maxrow=n-1;
+        int maxcol=n-1;
+
+        while(count <= n*n){
+
+            //  minrow constant ,mincol->maxcol 
+            for(int a=mincol;a<=maxcol;a++){
+                arr[minrow][a]=count;
+                count++;
+            }
+
+            //maxcol constant ,minrow+1 ->>maxrow
+            for(int b=minrow+1;b<=maxrow;b++){
+                arr[b][maxcol]=count;
+                count++;
+            }
+
+            //maxrow constant ,maxcol-1  ->>mincol
+            for(int a=maxcol-1;a>=mincol;a--){
+                arr[maxrow][a]=count;
+                count++;
+            }
+
+            //mincol constant, maxrow-1 ->>minrow+1
+            for(int b=maxrow-1;b>=minrow+1;b--){
+                arr[b][mincol]=count;
+                count++;
+            }
+            //increase min++,and decrease max--
+            minrow++;
+            mincol++;
+            maxrow--;
+            maxcol--;
+        }
+        return arr;
+    }
+    public static void main(String[] args){
+
+      int number=3;
+      int[][]result=generateMatrix(number);
+
+      //traverse 2d array
+       for(int i=0;i<result.length;i++){
+        for(int j=0;j<result[i].length;j++){
+            System.out.print(result[i][j]+" ");
+        }
+        System.out.print(" ");
+      }
+
+    }
+}
