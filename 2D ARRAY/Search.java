5841ff11c208846342581b59a7181b898f5dd59a
veer <vinaykumar24427@gmail.com>
2025-10-02 11:31:36 +0530

    Search_In_2d_Matrix

---
+++ 2D ARRAY/Search.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Search{
+    public static boolean searchMatrix(int [][] matrix,int target){
+        int row=0;
+        int col=matrix[0].length-1;
+
+        while(row<matrix.length  && col>=0){
+            if(matrix[row][col]==target){
+                return true;
+            }
+            else if(matrix[row][col]>target){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args){
+        int[][] arr={{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        int targ=3;
+        boolean result=searchMatrix(arr,targ);
+        System.out.println(result);
+
+    }
+}
