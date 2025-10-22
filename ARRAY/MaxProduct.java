22ef067fdae60c417c469c1070d405159a21bf50
veer <vinaykumar24427@gmail.com>
2025-10-22 20:38:25 +0530

    Max_Product

---
+++ ARRAY/MaxProduct.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class MaxProduct{
+    public static int maxProduct(int []nums){
+    int max=-1;
+    int smax=-1;
+    for(int i=0;i<nums.length;i++){
+        if(max<nums[i]){
+            smax=max;
+            max=nums[i];
+        }
+        else if(smax<nums[i]){
+            smax=nums[i];
+        }
+    }
+    return (max-1)*(smax-1);
+    }
+    public static void main(String[] args){
+        int [] arr={1,3,4,5};
+        System.out.println(maxProduct(arr));
+    }
+}
