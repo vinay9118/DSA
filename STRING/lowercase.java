5a4df7487386f97c67e680f7357e94eab3a3769a
veer <vinaykumar24427@gmail.com>
2025-10-27 16:48:13 +0530

    toLowercase

---
+++ STRING/lowercase.java
@@ -0,0 +1,9 @@
+public class lowercase{
+    public static String  toLowerCase(String s){
+        return s.toLowerCase();
+    }
+    public static void main(String[] args){
+        String s = "Hello";
+        System.out.println(toLowerCase(s));
+    }
+}
