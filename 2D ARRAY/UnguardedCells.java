f49e5940de798b7e4a91ad6f3c031eccf44c036d
veer <vinaykumar24427@gmail.com>
2025-11-02 18:12:43 +0530

    UnguardedCells

---
+++ 2D ARRAY/UnguardedCells.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class UnguardedCells {
+    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+        int[][] grid = new int[m][n];
+        
+        // 1 = guard, 2 = wall, 3 = guarded
+        for (int[] g : guards) grid[g[0]][g[1]] = 1;
+        for (int[] w : walls) grid[w[0]][w[1]] = 2;
+        
+        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+        
+        for (int[] g : guards) {
+            int r = g[0], c = g[1];
+            for (int[] d : dirs) {
+                int nr = r + d[0], nc = c + d[1];
+                while (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] != 1 && grid[nr][nc] != 2) {
+                    if (grid[nr][nc] == 0) grid[nr][nc] = 3;  // mark as guarded
+                    nr += d[0];
+                    nc += d[1];
+                }
+            }
+        }
+        
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) count++;
+            }
+        }
+        
+        return count;
+    }
+
+    public static void main(String[] args) {
+        UnguardedCells sol = new UnguardedCells();
+        int m = 4, n = 6;
+        int[][] guards = {{0,0},{1,1},{2,3}};
+        int[][] walls = {{0,1},{2,2},{1,4}};
+        System.out.println(sol.countUnguarded(m, n, guards, walls));  // Output: 7
+    }
+}
