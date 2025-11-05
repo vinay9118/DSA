73c069c2a1d2087ce9a30a7d090098f33b9e2dec
veer <vinaykumar24427@gmail.com>
2025-11-05 10:18:06 +0530

    h_index

---
+++ ARRAY/hindex.java
@@ -0,0 +1,32 @@
+public class hindex {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] counts = new int[n + 1];
+
+        for (int c : citations) {
+            if (c >= n) counts[n]++;
+            else counts[c]++;
+        }
+
+        int papersSoFar = 0;
+        for (int h = n; h >= 0; h--) {
+            papersSoFar += counts[h];
+            if (papersSoFar >= h) return h;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // Example input
+        int[] citations = {3, 0, 6, 1, 5};
+
+        // Create an object of hindex class
+        hindex obj = new hindex();
+
+        // Call hIndex() using the object
+        int result = obj.hIndex(citations);
+
+        // Print the result
+        System.out.println("H-Index: " + result);
+    }
+}
