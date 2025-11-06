e1a90ec004d5e0092a2b4a42e300c27967be7e04
veer <vinaykumar24427@gmail.com>
2025-11-06 10:15:29 +0530

    SearchInsertPosition

---
+++ ARRAY/SearchInsertPosition.java
@@ -0,0 +1,31 @@
+public class SearchInsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return start;
+    }
+
+    public static void main(String[] args) {
+        // Example input (you can change this)
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+
+        int position = searchInsert(nums, target);
+
+        System.out.println("Target: " + target);
+        System.out.println("Insert position: " + position);
+    }
+}
