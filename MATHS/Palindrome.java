3c077dc6da7db257baa40022b5d948379a4567f4
veer <vinaykumar24427@gmail.com>
2025-11-10 18:31:48 +0530

    Palindrome_Number

---
+++ MATHS/Palindrome.java
@@ -0,0 +1,25 @@
+public class Palindrome{
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int n = x;
+        int reverse = 0;
+        while (n > 0) {
+            int d = n % 10;
+            reverse = reverse * 10 + d;
+            n = n / 10;
+        }
+        return reverse == x;
+    }
+
+    public static void main(String[] args) {
+        // Example input (you can change it)
+        int x = 121;
+
+        // Call the function and print output
+        boolean result = isPalindrome(x);
+        System.out.println("Input number: " + x);
+        System.out.println("Is palindrome? " + result);
+    }
+}
