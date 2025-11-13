5ed7c4da3f57499f5fb71aec435f05cac294fc10
veer <vinaykumar24427@gmail.com>
2025-11-13 08:11:14 +0530

    Power_Of_Two

---
+++ MATHS/poweroftwo.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class poweroftwo{
+    public static boolean Poweroftwo(int n){
+        if(n<1){
+            return false;
+        }
+        else if(n==1){
+            return true;
+        }
+        else{
+            while(n%2==0){
+                n=n/2;
+            }
+            if(n==1){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number:");
+        int n=sc.nextInt();
+        boolean result=Poweroftwo(n);
+        System.out.print(result); 
+    }
+}
