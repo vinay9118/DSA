ef7e66762437bc260a0f622a7c072b5b959fcfab
veer <vinaykumar24427@gmail.com>
2025-11-20 13:19:58 +0530

    Update the code

--- MATHS/Countoperation.java
+++ MATHS/Countoperation.java
@@ -1,15 +1,15 @@
 public class Countoperation {
     public static int countOperations(int num1, int num2) {
-        int count = 0;
+        int countans = 0;
         while (num1 != 0 && num2 != 0) {
             if (num1 >= num2) {
                 num1 = num1 - num2;
             } else {
                 num2 = num2 - num1;
             }
-            count++;
+            countans++;
         }
-        return count;
+        return countans;
     }
 
     public static void main(String[] args) {
