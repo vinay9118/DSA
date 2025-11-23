5f467f6fa87deb002c181396400b1e854e598971
veer <vinaykumar24427@gmail.com>
2025-11-23 21:44:33 +0530

    Redistribute Characters to Make All Strings Equal

---
+++ ARRAY/makeEqual.java
@@ -0,0 +1,28 @@
+
+class makeEqual {
+
+    public static boolean makeEqual(String[] words) {
+        int[] f = new int[26];
+
+        for (String w : words) {
+            for (char ch : w.toCharArray()) {
+                f[ch - 'a']++;
+            }
+        }
+
+        for (int count : f) {
+            if (count % words.length != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String[] words = {"abc", "aabc", "bc"};  // Input already defined
+
+        boolean result = makeEqual(words);
+        System.out.println(result);
+    }
+}
