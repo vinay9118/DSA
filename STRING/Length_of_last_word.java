7b973485f31dc0390e6ca19397f83c9f7c439b48
veer <vinaykumar24427@gmail.com>
2025-11-24 20:14:06 +0530

    Update

--- STRING/Length_of_last_word.java
+++ STRING/Length_of_last_word.java
@@ -2,17 +2,17 @@
 public class Length_of_last_word {
 
     public static int lengthOfLastWord(String s) {
-        int count = 0;
+        int countnumber = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
             char ch = s.charAt(i);
 
             if (ch != ' ') {
-                count++;
-            } else if (count != 0) {
+                countnumber++;
+            } else if (countnumber != 0) {
                 break;
             }
         }
-        return count;
+        return countnumber;
     }
 
     public static void main(String[] args) {
