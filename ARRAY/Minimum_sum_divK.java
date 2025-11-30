9bc404ff71af9539ce2de0f13adaca5e5d7017ac
veer <vinaykumar24427@gmail.com>
2025-11-30 16:57:35 +0530

    Minimum sum div by k

--- ARRAY/Minimum_sum_divK.java
+++ ARRAY/Minimum_sum_divK.java
@@ -2,17 +2,16 @@
 public class Minimum_sum_divK {
 
     public static int minOperations(int[] nums, int k) {
-        long sum = 0;
+        long s = 0;
         for (int x : nums) {
-            sum += x;
+            s += x;
         }
-        return (int) (sum % k);
+        return (int) (s % k);
     }
 
     public static void main(String[] args) {
         int[] arr = {3, 9, 7};
         int k = 5;
-        System.out.println(minOperations(arr,k));
+        System.out.println(minOperations(arr, k));
     }
 }
-
