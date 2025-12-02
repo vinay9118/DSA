0282271e3e18179c510e447cb00427777f3c2140
veer <vinaykumar24427@gmail.com>
2025-12-02 21:39:41 +0530

    Sor Colors

--- SORTING/sort.java
+++ SORTING/sort.java
@@ -1,34 +1,36 @@
 import java.util.*;
-//leetcode 75
-public class sort{
-    public static int sortColors(int[] nums){
-      int n=nums.length;
-      int mid=0;
-      int low=0;
-      int high=n-1;
 
-      while(mid<=high){
-        if(nums[mid] == 0){
-            int temp=nums[low];
-            nums[low]=nums[mid];
-            nums[mid]=temp;
-            mid++;
-            low++;
-        }
-        else if(nums[mid]==1){
-            mid++;
-        }
-        else{
-            int temp=nums[high];
-            nums[high]=nums[mid];
-            nums[mid]=temp;
+// leetcode 75
+public class sort {
 
-        }
+    public static void sortColors(int[] nums) {
+        int n = nums.length;
+        int low = 0, mid = 0, high = n - 1;
 
-      } 
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } 
+            else if (nums[mid] == 1) {
+                mid++;
+            } 
+            else { // nums[mid] == 2
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;     // FIXED
+            }
+        }
     }
-    public static void main(String[] args){
-        int [] num={2,0,2,1,1,0,1,2,0,1};
-        System.out.println(singleNonDuplicate(num));
+
+    public static void main(String[] args) {
+        int[] num = {2,0,2,1,1,0,1,2,0,1};
+        sortColors(num);  // this sorts the array
+
+        System.out.println(Arrays.toString(num));
     }
 }
