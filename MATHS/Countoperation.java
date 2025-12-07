3e778469e5823efe45ad598e0d4802ce9b7ac967
veer <vinaykumar24427@gmail.com>
2025-12-07 11:49:57 +0530

    Count Operation

--- MATHS/Countoperation.java
+++ MATHS/Countoperation.java
@@ -13,9 +13,9 @@ public class Countoperation {
     }
 
     public static void main(String[] args) {
-        int num1 =10;
+        int num =10;
         int num2=10;
-        int result = countOperations(num1, num2);
+        int result = countOperations(num, num2);
         System.out.println(result);
     }
 }
