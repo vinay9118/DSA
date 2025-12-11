f4ce5a68f06f4d99ae3874cf4bbfdcd840eda566
veer <vinaykumar24427@gmail.com>
2025-12-11 17:36:10 +0530

    Palindrome Number

--- MATHS/Palindrome.java
+++ MATHS/Palindrome.java
@@ -1,25 +1,30 @@
-public class Palindrome{
+public class Palindrome {
     public static boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
+        if (x < 0) return false;            
+        if (x >= 0 && x < 10) return true;  
+        int div = 1;
+        while (x / div >= 10) {
+            div *= 10;
         }
-        int n = x;
-        int rev = 0;
-        while (n > 0) {
-            int d = n % 10;
-            rev = rev * 10 + d;
-            n = n / 10;
+        while (x > 0) {
+            int left = x / div;   
+            int right = x % 10;  
+            if (left != right) return false;
+            // Remove leftmost and rightmost digits
+            x = (x % div) / 10;  
+            div /= 100;  // shrinking divisor since we removed 2 digits
         }
-        return rev == x;
+
+        return true;
     }
 
     public static void main(String[] args) {
-        // Example input (you can change it)
         int x = 121;
 
-        // Call the function and print output
         boolean result = isPalindrome(x);
         System.out.println("Input number: " + x);
         System.out.println("Is palindrome? " + result);
     }
 }
+
+
