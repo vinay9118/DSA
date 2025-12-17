b875d73162d392d43362eb9897d1fab630efc7ec
veer <vinaykumar24427@gmail.com>
2025-12-17 10:21:45 +0530

    Sort

--- SORTING/sort.java
+++ SORTING/sort.java
@@ -29,7 +29,7 @@ public class sort {
 
     public static void main(String[] args) {
         int[] num = {2,0,2,1,1,0,1,2,0,1};
-        sortColors(num);  // this sorts the array
+        sortColors(num);  
 
         System.out.println(Arrays.toString(num));
     }
