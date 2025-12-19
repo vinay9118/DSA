5a32ca362d09313e5d375c7429b7c6e2691c749f
veer <vinaykumar24427@gmail.com>
2025-12-19 13:11:28 +0530

    Update the sorting

--- SORTING/sort.java
+++ SORTING/sort.java
@@ -28,9 +28,8 @@ public class sort {
     }
 
     public static void main(String[] args) {
-        int[] num = {2,0,2,1,1,0,1,2,0,1};
+        int[] num = {2,0,2,1,1,0,1,2,1};
         sortColors(num);  
-
         System.out.println(Arrays.toString(num));
     }
 }
