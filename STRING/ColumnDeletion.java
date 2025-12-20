74131645738941c47fadef956df577500bbd6797
veer <vinaykumar24427@gmail.com>
2025-12-20 12:29:12 +0530

    ColumnDeletion

---
+++ STRING/ColumnDeletion.java
@@ -0,0 +1,29 @@
+
+public class ColumnDeletion {
+
+    public static int minDeletionSize(String[] strs) {
+        int rows = strs.length;
+        int cols = strs[0].length();
+        int count = 0;
+
+        for (int c = 0; c < cols; c++) {
+            for (int r = 0; r < rows - 1; r++) {
+                if (strs[r].charAt(c) > strs[r + 1].charAt(c)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        // Input already defined
+        String[] strs = {"abc", "bce", "cae"};
+
+        int result = minDeletionSize(strs);
+
+        // Output
+        System.out.println("Number of columns to delete: " + result);
+    }
+}
