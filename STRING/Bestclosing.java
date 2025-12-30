860db1a97acfe61d20611e0e950c501ccbcc7ec3
veer <vinaykumar24427@gmail.com>
2025-12-30 21:35:03 +0530

    Update

--- STRING/Bestclosing.java
+++ STRING/Bestclosing.java
@@ -27,7 +27,6 @@ public class Bestclosing{
     }
 
     public static void main(String[] args) {
-        // Input already defined
         String customers = "YYN";
 
         int result = bestClosingTime(customers);
