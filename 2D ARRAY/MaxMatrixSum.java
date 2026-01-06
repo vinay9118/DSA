ea0365085fe307ced53c654939733a8bbc46c4df
veer <vinaykumar24427@gmail.com>
2026-01-06 13:16:04 +0530

    Update

---
+++ 2D ARRAY/MaxMatrixSum.java
@@ -0,0 +1,37 @@
+
+public class MaxMatrixSum {
+
+    public static long maxMatrixSum(int[][] matrix) {
+        long totalSum = 0;
+        int negativeCount = 0;
+        int minAbs = Integer.MAX_VALUE;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                int val = matrix[i][j];
+                totalSum += Math.abs(val);
+
+                if (val < 0) {
+                    negativeCount++;
+                }
+
+                minAbs = Math.min(minAbs, Math.abs(val));
+            }
+        }
+
+        if (negativeCount % 2 == 1) {
+            totalSum -= 2L * minAbs;
+        }
+
+        return totalSum;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, -1},
+            {-1, 1}
+        };
+        long result = maxMatrixSum(matrix);
+        System.out.println("Maximum Matrix Sum: " + result);
+    }
+}
