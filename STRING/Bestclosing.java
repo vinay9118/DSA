b97c647d5f00b033040c79876e63ae87e2a43fa0
veer <vinaykumar24427@gmail.com>
2026-01-07 10:03:10 +0530

    Update

--- STRING/Bestclosing.java
+++ STRING/Bestclosing.java
@@ -28,9 +28,7 @@ public class Bestclosing{
 
     public static void main(String[] args) {
         String cust= "YYN";
-
         int result = bestClosingTime(cust);
-
         System.out.println("Customer Log: " + cust);
         System.out.println("Best Closing : " + result);
     }
