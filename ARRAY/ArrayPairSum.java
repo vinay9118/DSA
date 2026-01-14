2ebe8abd5ec6762696bbf74cc50d0e4d88ea916a
veer <vinaykumar24427@gmail.com>
2026-01-14 09:18:18 +0530

    Array Partition

---
+++ ARRAY/ArrayPairSum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class ArrayPairSum{
+
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        // Input already defined
+        int[] nums = {1, 4, 3, 2};
+
+        int result = arrayPairSum(nums);
+
+        // Output
+        System.out.println("Maximum sum of min pairs: " + result);
+    }
+}
