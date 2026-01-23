ea811539a4075af69a1914c96eaf6a222221a7d0
veer <vinaykumar24427@gmail.com>
2026-01-23 16:33:11 +0530

    UPDATE

--- 2D ARRAY/SeparateSquares.java
+++ 2D ARRAY/SeparateSquares.java
@@ -46,6 +46,6 @@ public class SeparateSquares{
         };
 
         double result = separateSquares(squares);
-        System.out.printf("Minimum y-cor", result);
+        System.out.printf("result", result);
     }
 }
