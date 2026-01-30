e46a4f964d8ab9e585c8b7471d4627ac6e4384c4
veer <vinaykumar24427@gmail.com>
2026-01-30 21:47:22 +0530

    UPDATE

--- 2D ARRAY/SpiralMatrix.java
+++ 2D ARRAY/SpiralMatrix.java
@@ -50,7 +50,6 @@ public class SpiralMatrix{
       int number=3;
       int[][]result=generateMatrix(number);
 
-      //traverse 2d array
        for(int i=0;i<result.length;i++){
         for(int j=0;j<result[i].length;j++){
             System.out.print(result[i][j]+" ");
