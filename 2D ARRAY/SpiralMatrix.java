8bd223403777172a30e2a7588f6e1024fbd9435d
veer <vinaykumar24427@gmail.com>
2026-02-02 16:32:44 +0530

    UPDATE

--- 2D ARRAY/SpiralMatrix.java
+++ 2D ARRAY/SpiralMatrix.java
@@ -35,7 +35,7 @@ public class SpiralMatrix{
                 arr[b][mincol]=count;
                 count++;
             }
-            //increase min++,and decrease max--
+    
             minrow++;
             mincol++;
             maxrow--;
