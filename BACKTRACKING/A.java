571e3516af5c0cba841f8a0521bb9c6178dfad12
veer <vinaykumar24427@gmail.com>
2026-02-12 16:03:10 +0530

    Simple Backtracking Question

---
+++ BACKTRACKING/A.java
@@ -0,0 +1,26 @@
+// 1 2 3 4 5 -> -1 0 1 2 3
+
+public class A{
+    public static void changeArr(int []arr,int i,int val){
+        //base case
+        if(i== arr.length){
+            printArr(arr);
+            return;
+        }
+        //recursion
+        arr[i]=val;
+        changeArr(arr, i+1, val+1);
+        arr[i]=arr[i]-2;
+    }
+
+    public static void printArr(int [] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(" "+arr[i]);
+        }
+    }
+    public static void main(String [] args){
+        int []arr=new int[5];// 1 2 3 4 5 -> -1 0 1 2 3
+        changeArr(arr,0,1);
+        printArr(arr);
+    }
+}
