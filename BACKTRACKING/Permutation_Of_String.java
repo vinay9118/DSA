2d32283eda49a2dbdde21628da9e25008e7554b1
veer <vinaykumar24427@gmail.com>
2026-02-13 08:47:11 +0530

    Permutation of String

---
+++ BACKTRACKING/Permutation_Of_String.java
@@ -0,0 +1,19 @@
+public class Permutation_Of_String{
+    public static void findPermutation(String str,String ans){
+        //Base Case
+        if(str.length()==0){
+            System.out.print(ans);
+            return;
+        }
+        //recursive call -
+        for(int i=0;i<str.length();i++){
+            char curr=str.charAt(i);
+           String newstr=str.substring(0,i)+str.substring(i+1);
+            findPermutation(newstr, ans+curr);
+        }
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        findPermutation(str," ");
+    }
+}
