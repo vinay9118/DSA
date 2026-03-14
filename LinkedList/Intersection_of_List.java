aaaabc92e9a390b799d89c7013727bae407097f7
veer <vinaykumar24427@gmail.com>
2026-03-14 10:50:34 +0530

    Update

--- LinkedList/Intersection_of_List.java
+++ LinkedList/Intersection_of_List.java
@@ -65,7 +65,7 @@ public class Intersection_of_List {
     }
 
     public static void main(String[] args) {
-        //create First List
+        //create First 
         ListNode head1 = new ListNode(1);
         ListNode temp1 = new ListNode(2);
         head1.next = temp1;
