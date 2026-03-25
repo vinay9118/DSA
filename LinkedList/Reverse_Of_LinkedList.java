d578994fe6b6b1f0a406e1094e68547162e7ce08
veer <vinaykumar24427@gmail.com>
2026-03-25 16:16:26 +0530

    Reverse Of Linked List

---
+++ LinkedList/Reverse_Of_LinkedList.java
@@ -0,0 +1,59 @@
+
+public class Reverse_Of_LinkedList {
+
+    public class Node {
+
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static int size;
+    public void print(){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" -> ");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+    //Reverse Order of Linked List
+    public void reverse(){
+        Node prev=null;
+        Node curr=tail=head;
+        Node next;
+
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head=prev;
+       
+    }
+
+    public static void main(String[] args) {
+        Reverse_Of_LinkedList ll = new Reverse_Of_LinkedList();
+        for(int i=5;i>0;i--){
+            ll.add(i);
+        }
+        ll.print();
+        ll.reverse();
+        ll.print();
+    }
+}
