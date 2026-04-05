1f1cd25d8839b32d018440b91a718d22663348da
veer <vinaykumar24427@gmail.com>
2026-04-05 21:09:31 +0530

    Update

--- LinkedList/Searching_LinkedList.java
+++ LinkedList/Searching_LinkedList.java
@@ -58,7 +58,7 @@ public class Searching_LinkedList {
             ll.add(i);
         }
         ll.Print();
-        //Searching in LinkedList
+        //Searching operation 
         ll.search(30);
     }
 
