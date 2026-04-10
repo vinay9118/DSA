e53a47d4c8f79a4308b2dc5dfc0d5f213800208b
veer <vinaykumar24427@gmail.com>
2026-04-10 12:33:27 +0530

    Added solution for Minimum Sum Divisible by K

--- ARRAY/Minimum_sum_divK.java
+++ ARRAY/Minimum_sum_divK.java
@@ -10,6 +10,7 @@ public class Minimum_sum_divK {
     }
 
     public static void main(String[] args) {
+        //Input
         int[] arr = {3, 9, 4};
         int k = 5;
         System.out.println(minOperations(arr, k));
