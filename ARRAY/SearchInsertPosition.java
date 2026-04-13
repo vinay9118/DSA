f34b5022304041c29ec344d79192c0ff3d752809
veer <vinaykumar24427@gmail.com>
2026-04-13 10:51:36 +0530

    Added solution for Search Insert Position

--- ARRAY/SearchInsertPosition.java
+++ ARRAY/SearchInsertPosition.java
@@ -19,7 +19,7 @@ public class SearchInsertPosition {
     }
 
     public static void main(String[] args) {
-        // Example input (you can change this)
+        // Example input
         int[] nums = {1, 3, 5, 6};
         int target = 5;
 
