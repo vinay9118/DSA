82538ee0678ce17743337012c04fab8aafcc4d5d
veer <vinaykumar24427@gmail.com>
2026-04-16 10:27:40 +0530

    Added solution for Two Sum II - Input Array Is Sorted

--- ARRAY/TwoSum167.java
+++ ARRAY/TwoSum167.java
@@ -23,7 +23,6 @@ import java.util.*;
         return ans;
     }
     public static void main(String[] args){
-        //Input
         int[]  arr={2,7,11,15};
         int[]result=twoSum(arr,9);
         System.out.println(Arrays.toString(result));
