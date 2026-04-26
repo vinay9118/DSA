9179cf7c27b4008535f88f69b5473daddc289098
veer <vinaykumar24427@gmail.com>
2026-04-26 09:49:04 +0530

    Added solution for Power of Two (LeetCode 231)

--- MATHS/poweroftwo.java
+++ MATHS/poweroftwo.java
@@ -1,29 +1,31 @@
+
 import java.util.*;
-public class poweroftwo{
-    public static boolean Poweroftwo(int n){
-        if(n<1){
+
+public class poweroftwo {
+
+    public static boolean Poweroftwo(int n) {
+        if (n < 1) {
             return false;
-        }
-        else if(n==1){
+        } else if (n == 1) {
             return true;
-        }
-        else{
-            while(n%2==0){
-                n=n/2;
+        } else {
+            while (n % 2 == 0) {
+                n = n / 2;
             }
-            if(n==1){
+            if (n == 1) {
                 return true;
-            }
-            else{
+            } else {
                 return false;
             }
         }
     }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+
+    public static void main(String[] args) {
+        //Take Input From User
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter the Number:");
-        int n=sc.nextInt();
-        boolean result=Poweroftwo(n);
-        System.out.print(result); 
+        int n = sc.nextInt();
+        boolean result = Poweroftwo(n);
+        System.out.print(result);
     }
 }
