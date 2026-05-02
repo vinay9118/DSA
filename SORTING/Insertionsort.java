c7728b5bfd7332624583778aa0c0b696b8644c22
veer <vinaykumar24427@gmail.com>
2026-05-02 11:19:37 +0530

    Added solution for Insertion Sort algorithm

--- SORTING/Insertionsort.java
+++ SORTING/Insertionsort.java
@@ -1,25 +1,28 @@
+
 import java.util.*;
 
-public class Insertionsort{
-    public static int [] insertionsort(int[] num){
+public class Insertionsort {
+
+    public static int[] insertionsort(int[] num) {
         //outer loop   
-        for(int i=1;i<num.length;i++){
-            int cur=num[i];
-            int prev=i-1;
-            
-            
-            while(prev >=0 && num[prev]>cur){
-                num[prev+1]=num[prev];
+        for (int i = 1; i < num.length; i++) {
+            int cur = num[i];
+            int prev = i - 1;
+
+            while (prev >= 0 && num[prev] > cur) {
+                num[prev + 1] = num[prev];
                 prev--;
             }
 
-            num[prev+1]=cur;
+            num[prev + 1] = cur;
         }
         return num;
     }
-    public static void main(String[] args){
-        int arr[]={3,6,2,1,8,7,4,5,3};
-        int[]result=insertionsort(arr);
+
+    public static void main(String[] args) {
+        //Input
+        int arr[] = {3, 6, 2, 1, 8, 7, 4, 5, 3};
+        int[] result = insertionsort(arr);
         System.out.println(Arrays.toString(result));
     }
 }
