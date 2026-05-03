fbcf4d5983616736f113790976e9fe8294a9b873
veer <vinaykumar24427@gmail.com>
2026-05-03 12:24:10 +0530

    Added solution for Consecutive Characters (LeetCode 1446)

--- STRING/ConsecutiveCharacter.java
+++ STRING/ConsecutiveCharacter.java
@@ -1,26 +1,27 @@
-import java.util.*;
 
-public class ConsecutiveCharacter{
-    public static int maxPower(String s){
-        int count=1;
-        int max=1;
-        for(int i=1; i<s.length();i++){
-          int curr=s.charAt(i);
-          int prev=s.charAt(i-1);
-          if(curr == prev){
-            count++;
-          }
-          else{
-            max=Math.max(max,count);
-            count=1;
-          }
-        }  
-        max=Math.max(max,count);
+public class ConsecutiveCharacter {
+
+    public static int maxPower(String s) {
+        int count = 1;
+        int max = 1;
+        for (int i = 1; i < s.length(); i++) {
+            int curr = s.charAt(i);
+            int prev = s.charAt(i - 1);
+            if (curr == prev) {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 1;
+            }
+        }
+        max = Math.max(max, count);
         return max;
     }
-    public static void main(String[]args){
-          String s="leetcooode";
-          System.out.println(maxPower(s));
+
+    public static void main(String[] args) {
+        //Input
+        String s = "leetcooode";
+        System.out.println(maxPower(s));
 
     }
 }
