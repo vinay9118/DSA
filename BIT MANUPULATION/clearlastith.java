16b319df1411d4f7f29c7240f67acf3a24afe66f
veer <vinaykumar24427@gmail.com>
2026-05-04 10:38:32 +0530

    Added solution to clear the last i bits of a number using bitwise operations

---
+++ BIT MANUPULATION/clearlastith.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class clearlastith{
+   
+   //DECIMAL TO BINARY CONVERTER
+    public static String Dectobin(int n){
+        if(n==0){
+            return "0";
+        }
+        String result="" ;
+        while(n>0){
+            int re=n%2;
+            result=re+result;
+            n=n/2;
+        }
+        
+       return result; 
+    }
+    
+    public static int Clearlastith(int n,int i){
+        
+    // replace zero with all palces except i th position 
+          int bitmask=(~0)<<i;
+
+    //for checking ith place zero or one
+          return (n & bitmask);
+    }
+    public static void main(String[] args){
+        int num=15;
+        System.out.println("Binary Number of First Number:::"+Dectobin(num));
+        System.out.println(Clearlastith(num,2));
+    }
+
+}
