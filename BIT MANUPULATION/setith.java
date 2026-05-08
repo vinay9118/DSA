a1de117bd5b1f1f80ebb22c7829f0d205c1baa11
veer <vinaykumar24427@gmail.com>
2026-05-08 22:09:45 +0530

    Added solution to set the i-th bit of a number using bitwise operations

---
+++ BIT MANUPULATION/setith.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class setith{
+   
+   //DECIMAL TO BINARY CONVERTER
+    public static String Dectobin(int n){
+        if(n==0){
+            return "0";
+        }
+        String result="" ;
+        while(n>0){
+            int re=n%2;
+            result=re+result;
+            n=n/2;
+        }
+        
+       return result; 
+    }
+    
+    public static int Setith(int n,int i){
+
+    // replace zero with all palces except i th position 
+          int bitmask=1<<i;
+
+    //for checking ith place zero or one
+          return (n | bitmask);
+    }
+    public static void main(String[] args){
+        int num=10;
+        System.out.println("Binary Number of First Number:::"+Dectobin(num));
+        System.out.println(Setith(num,2));
+
+        
+    }
+
+}
