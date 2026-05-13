3483730f1b947a72dd94d560f61ca6ca44d8208e
veer <vinaykumar24427@gmail.com>
2026-05-13 09:52:20 +0530

    Added solution to print an alphabet square pattern

---
+++ PATTERN/AlphabetSquare.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class AlphabetSquare{
+   
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        int n=sc.nextInt();
+        for( int i=1;i<=n;i++){
+            int a=65;
+            for(int j=1;j<=n;j++){
+                System.out.print((char)a);
+                a=a+1;
+            }
+             System.out.println(" ");
+        }
+        
+    }
+}
