535852aa79598cc0356704c75c85f2ea93516021
veer <vinaykumar24427@gmail.com>
2026-05-13 09:55:02 +0530

    Added solution to print a butterfly star pattern

---
+++ PATTERN/butter.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class butter{
+   
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        // System.out.print("Enter the first number:");
+        // int n=sc.nextInt();
+        int n=4;
+        for( int i=1;i<=n;i++){
+          for(int j=1;j<=i;j++){
+            System.out.print("*");
+          }
+         for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+          }
+        for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+          }
+         for(int j=1;j<=i;j++){
+            System.out.print("*");
+          }    
+      System.out.println(" ");
+      }
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=n+1-i;j++){
+            System.out.print("*");
+        }
+        for(int j=1;j<=i-1;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=i-1;j++){
+            System.out.print(" ");
+        }
+        for(int j=1;j<=n+1-i;j++){
+            System.out.print("*");
+        }
+        System.out.println(" ");
+      }
+        
+    }
+}
