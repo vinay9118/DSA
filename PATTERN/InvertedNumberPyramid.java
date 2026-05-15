a9238f6423b62139229913a6a46af0892b65b5c6
veer <vinaykumar24427@gmail.com>
2026-05-15 09:53:16 +0530

    Added solution to print an inverted half-pyramid with numbers

---
+++ PATTERN/InvertedNumberPyramid.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class InvertedNumberPyramid{
+   
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        // *
+        // **
+        // ***
+        int n=sc.nextInt();
+        for( int i=1;i<=n;i++){
+            for(int j=1;j<=n+1-i;j++){
+                System.out.print(j);
+            }
+             System.out.println(" ");
+        }
+        
+    }
+}
