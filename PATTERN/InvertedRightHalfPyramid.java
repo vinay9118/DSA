e21912f8245ee9f880ebff50149b5c6e21aa8981
veer <vinaykumar24427@gmail.com>
2026-05-16 17:54:53 +0530

    Added solution to print an inverted right half-pyramid star pattern

---
+++ PATTERN/InvertedRightHalfPyramid.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class InvertedRightHalfPyramid{
+   
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        int n=sc.nextInt();
+        for( int i=1;i<=n;i++){
+            for(int j=1;j<=n+1-i;j++){
+                System.out.print("*");
+            }
+             System.out.println("");
+        }
+        
+    }
+}
