9172ba2335ea66fd4780ab749fe30b5b48e8862d
veer <vinaykumar24427@gmail.com>
2026-05-18 09:51:43 +0530

    Added solution to print a star cross pattern

---
+++ PATTERN/starcross.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class starcross{
+   
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        int n=sc.nextInt();
+        
+        for( int i=1;i<=n;i++){
+          for(int j=1;j<=n;j++){
+            if(i+j==n+1 ||i-j==0){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+            
+          }
+           System.out.println(" ");
+        }
+        
+    }
+}
