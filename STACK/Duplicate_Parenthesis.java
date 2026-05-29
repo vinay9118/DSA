2d020a29a05a245328e702b99f5666e680323edf
veer <vinaykumar24427@gmail.com>
2026-05-29 23:01:19 +0530

    Added Stack-based solution to detect duplicate parentheses in an expression

---
+++ STACK/Duplicate_Parenthesis.java
@@ -0,0 +1,35 @@
+
+import java.util.Stack;
+
+public class Duplicate_Parenthesis {
+
+    public static boolean duplicateParenthesis(String str) {
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            //closing
+            if (ch == ')') {
+                int count = 0;
+                while (s.peek() != '(') {
+                    s.pop();
+                    count++;
+                }
+                if (count < 1) {
+                    return true;
+                } else {
+                    s.pop();
+                }
+            } else {
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String str = "((a+b))";
+        System.out.print(duplicateParenthesis(str));
+    }
+}
