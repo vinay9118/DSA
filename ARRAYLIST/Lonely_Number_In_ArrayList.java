f6e043eb9407ba14e1b6de5c75175ed8d20a0f33
veer <vinaykumar24427@gmail.com>
2026-06-02 09:43:07 +0530

    Added solution to find lonely numbers in an ArrayList using sorting

---
+++ ARRAYLIST/Lonely_Number_In_ArrayList.java
@@ -0,0 +1,38 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Lonely_Number_In_ArrayList {
+
+    public static ArrayList<Integer> IsLonely(ArrayList<Integer> list) {
+        Collections.sort(list);
+        ArrayList<Integer>nums = new ArrayList<>();
+        for (int i = 1; i < list.size() - 1; i++) {
+            if (list.get(i - 1) + 1 < list.get(i )&& list.get(i) + 1 < list.get(i + 1)) {
+                nums.add(list.get(i));
+            }
+        }
+        if(list.size() ==1){
+             nums.add(list.get(0));
+        }
+        if(list.size()>1){
+            if(list.get(0)+1<list.get(1)){
+                nums.add(list.get(0));
+            }
+            if(list.get(list.size()-2)+1<list.get(list.size()-1)){
+                nums.add(list.get(list.size()-1));
+            }
+        }
+        System.out.println("Lonely Number is:"+nums);
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(6);
+        list.add(5);
+        list.add(8);
+        IsLonely(list);
+    }
+}
