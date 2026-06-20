40e2d420f3642fc48e01dadbfc5019bfc7dab3fb
veer <vinaykumar24427@gmail.com>
2026-06-20 23:44:56 +0530

    Added Greedy algorithm solution to find the minimum number of Indian coins for a given amount

---
+++ GREEDY ALGORITHM/Indian_Coins.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+
+public class Indian_Coins {
+
+    public static void main(String[] args) {
+        Integer[] coins = {1, 2, 5, 10, 20, 50, 100, 500, 2000};
+        Arrays.sort(coins, Comparator.reverseOrder());
+        int count = 0;
+        int amount = 590;
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < coins.length; i++) {
+            if (coins[i] <= amount) {
+                while (coins[i] <= amount) {
+                    count++;
+                    ans.add(coins[i]);
+                    amount -= coins[i];
+                }
+            }
+        }
+        System.out.println("Minimum coins are used:"+count+"  coins:"+ans);
+    }
+}
